865cfc8cd4e59ace91ac026107e0c9aab0cf2bc6
Sergey V <sergey.velikoretskikh@dasi.com>
2023-11-07 22:26:41 -0500

    working on categories

--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -1,23 +1,168 @@
 <script setup lang='ts'>
-import { useCategoryStore } from '~/stores/category.store';
+import { useCategoryStore, type Category } from '~/stores/category.store';
+import {FilterMatchMode} from 'primevue/api';
+import DataTable from 'primevue/datatable';
 
+// init store & get list
 const categoryStore = useCategoryStore();
-const fillCategories = () => {
-  categoryStore.getList(true);
+categoryStore.getList(false);
+
+// filters
+const filters = ref({
+  global: {value: null, matchMode: FilterMatchMode.CONTAINS}
+});
+
+// export
+const categoryTableRef = ref<DataTable>();
+const exportCSV = () => {
+  categoryTableRef.value?.exportCSV();
 }
 
-categoryStore.getList(false);
+// row edit
+const editingRows = ref<Category[]>([]);
+const save = () => {
+  console.log('-- save()');
+  // categoryStore.createOrUpdate(editingRows.value[0]);
+  console.log(categoryTableRef.value);
+  // categoryTableRef.value?.$emit.editingRows();
+}
+const onRowEditSave =({newData, index}: {newData: Category, index: number}) => {
+  console.log('-- onRowEditSave:', index, newData);
+}
+const tt = (event: any) => {
+  console.log('-- tt:', event)
+}
+const dd = (event: any) => {
+  console.log('-- dd:', event)
+}
+
+// save 
+const nameInput = ref();
+const addNew = () => {
+  categoryStore.categories.unshift({
+    name: '',
+    tags: [],
+  });
+  editingRows.value = [{...categoryStore.categories[0]}];
+  // nextTick(() => {
+  //   nameInput.value.$el.focus();
+  // });
+  setTimeout(() => {
+    nameInput.value.$el.focus();
+  }, 100);
+}
 </script>
 
 <template>
-  <h2>Categories Page</h2>
-  <hr>
-  <Button label="Reload" @click="fillCategories()" size="small"></Button>
-  <br>
-  <CategoryList></CategoryList>
+  <div>
 
-  <!-- <pre>
-    {{categoryStore.categories}}
-  </pre> -->
+    {{ editingRows }}
+
+    <!-- table -->
+    <DataTable
+    scrollable
+    @update:editingRows="tt"
+    @value-change="dd"
+    ref="categoryTableRef" 
+    :value="categoryStore.categories"
+    data-key="id"
+    key="id"
+    :striped-rows="true"
+    :filters="filters"
+    :global-filter-fields="['name', 'tags']"
+    :loading="categoryStore.isLoadingList || categoryStore.isCreating || categoryStore.isUpdating || categoryStore.isDeleting"
+    edit-mode="row"
+    v-model:editing-rows="editingRows"
+    @row-edit-save="onRowEditSave"
+    export-filename="Categories"
+    :pt="{
+          table: { style: 'min-width: 50rem' },
+          column: {
+              bodycell: ({ state }: any) => ({
+                  style:  state['d_editing']&&'padding-top: 0.6rem; padding-bottom: 0.6rem' 
+              })
+          }
+      }"
+    >
+
+      <!-- table header -->
+      <template #header>
+        <div class="flex justify-content-between">
+
+          <!-- table name -->
+          <div class="flex align-items-center justify-content-center text-xl text-900 font-bold">Categories</div>
+
+          <!-- search box  -->
+          <div class="flex align-items-center justify-content-center">
+            <span class="p-input-icon-left">
+              <i class="pi pi-search"/>
+              <InputText v-model="filters.global.value" placeholder="Search"/>
+            </span>
+          </div>
+
+          <!-- new & export btns -->
+          <div class="flex align-items-center justify-content-end">
+            <Button label="Add" icon="pi pi-plus" severity="success" class="mr-2" @click="addNew()"/>
+            <Button icon="pi pi-external-link" label="Export" @click="exportCSV()"/>
+          </div>
+        </div>
+      </template>
+
+      <!-- column: id -->
+      <Column field="id" header="ID" style="width: 4%; min-width: 2rem;"></Column>
+
+      <!-- column: name  -->
+      <Column field="name" header="Name" style="width: 25%">
+        <template #editor="{ data, field }">
+            <InputText v-model="data[field]" ref="nameInput"/>
+        </template>
+      </Column>
+
+      <!-- column: tags -->
+      <Column field="tags" header="Tags" style="width: 30%">
+        <template #body="{data, field}">
+          <Chip 
+            v-for="tag in data[field]" 
+            :key="tag"
+            :label="tag"
+            class="mr-1"
+          >{{ tag }}</Chip>
+        </template>
+        <template #editor="{ data, field }">
+          <Chips 
+          v-model="data[field]" 
+          separator="," 
+          :add-on-blur="true" 
+          :allow-duplicate="false"
+          />
+        </template>
+      </Column>
+      
+      <!-- column: actions -->
+      <Column :row-editor="true" style="width: 8%; min-width: 8rem;" frozen align-frozen="right">
+        <template #body="{data}">
+          <template v-if="editingRows.length && editingRows[0].id === data.id">
+
+            <!-- save icon -->
+            <Button icon="pi pi-check" size="small" text rounded severity="success" v-tooltip.top="'Save'" @click="save()"/>
+
+            <!-- cancel editing icon -->
+            <Button icon="pi pi-times" size="small" text rounded severity="secondary" v-tooltip.top="'Cancel'" @click="editingRows = []"></Button>
+
+          </template>
+          <template v-else>
+
+            <!-- edit icon -->
+          <Button icon="pi pi-pencil" size="small" text rounded severity="secondary" v-tooltip.top="'Edit'" @click="editingRows = [data]"/>
+
+          <!-- delete icon -->
+          <Button icon="pi pi-trash" size="small" text rounded severity="danger" v-tooltip.top="'Delete'" @click=""/>
+
+          </template>
+        </template>
+      </Column>
+
+    </DataTable>
+  </div>
 
 </template>
--- stores/category.store.ts
+++ stores/category.store.ts
@@ -1,11 +1,19 @@
 import {defineStore} from 'pinia';
+const baseUrl = 'http://localhost:3003';
 
-type Category = {
-  id: number;
+export interface Category {
+  id?: number;
   name: string;
+  tags: string[],
 }
 type State = {
   categories: Category[],
+  selectedCategory: Category | null,
+  isLoadingList: boolean,
+  isLoadingOne: boolean,
+  isCreating: boolean;
+  isUpdating: boolean;
+  isDeleting: boolean;
 }
 
 export const useCategoryStore = defineStore('CategoryStore', {
@@ -13,15 +21,74 @@ export const useCategoryStore = defineStore('CategoryStore', {
   state: (): State => {
     return {
       categories: [],
+      selectedCategory: null,
+      isLoadingList: false,
+      isLoadingOne: false,
+      isCreating: false,
+      isUpdating: false,
+      isDeleting: false,
     }
   },
 
   actions: {
-    async getList(forced: boolean) {
-      if (forced || !this.categories.length) {
-        const {data, error, pending}: any = await useFetch('http://localhost:3003/categories');
+    async getList(fromApi: boolean) {
+      this.isLoadingList = true;
+      if (fromApi || !this.categories.length) {
+        const {data, error, pending}: any = await useFetch(`${baseUrl}/categories`);
         this.categories = data.value || [];
       }
+      this.isLoadingList = false;
+    },
+    async getOne(id: number, fromApi: boolean) {
+      this.isLoadingOne = true;
+      if (!fromApi && this.categories.length) {
+        const foundCategory: Category | undefined = this.categories.find(c => c.id === id);
+        if (foundCategory) {
+          this.selectedCategory = foundCategory;
+          this.isLoadingOne = false;
+          return;
+        }
+      }
+      const {data, error, pending}: any = await useFetch(`${baseUrl}/categories/${id}`);
+      this.selectedCategory = data.value || null;
+      this.isLoadingOne = false;
+    },
+    async create(category: Category) {
+      this.isCreating = true;
+      const {data}: any = await useFetch(`${baseUrl}/categories`, {
+        method: 'post',
+        body: category
+      });
+      this.selectedCategory = data.value || null;
+      this.isCreating = false;
+      this.getList(true);
+    },
+    async update(category: Category) {
+      this.isUpdating = true;
+      const {data}: any = await useFetch(`${baseUrl}/categories/${category.id}`, {
+        method: 'put',
+        body: category,
+      });
+      this.selectedCategory = data.value || null;
+      this.getList(true);
+      this.isUpdating = false;
+    },
+    async createOrUpdate(category: Category) {
+      if (category.id && category.id > 0) {
+        await this.update(category);
+      } else {
+        await this.create(category);
+      }
+    },
+    async delete(id: number) {
+      this.isDeleting = true;
+      const {data}: any = await useFetch(`${baseUrl}/categories`, {
+        method: 'delete',
+        body: {id},
+      });
+      this.isDeleting = false;
+      this.selectedCategory = null;
+      this.getList(true);
     }
   },
 
